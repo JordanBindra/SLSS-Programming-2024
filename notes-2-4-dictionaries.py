376d44974bb8566cb55e8aa60191651dd9a4f3af
Jordan Bindra <jordanbindra@icloud.com>
2024-04-30 10:59:24 -0700

    completed

---
+++ notes-2-4-dictionaries.py
@@ -0,0 +1,20 @@
+# Dictionaries 
+# April 12 2024 
+
+# Big Ideas: 
+#   - dictionaires are date structure
+#   - dictionaries map keys to vaules 
+
+# Flashback to lists 
+person = [ "John", "23 years old", "4500 1023 2222 1111"]
+] 
+hj
+# Get and print the person's name 
+print(person[0])
+print(person[1])   # age? 
+print(person[2])   # some numbers? 
+
+# print the last thing in the list 
+print(person[-1])
+print(person[-2])  # second to last 
+# print(person[10])
